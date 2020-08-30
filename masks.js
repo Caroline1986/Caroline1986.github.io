1a89b15fa0e9aaa2ed7bd82dcb06b8cc3044b92f
Caroline E <caroline.esquivel01@gmail.com>
2020-08-29 23:21:18 -0500

    hover and scroll reveal effects

---
+++ masks.js
@@ -0,0 +1,6 @@
+
+
+ScrollReveal().reveal('.crafty', { delay: 4000});
+ScrollReveal().reveal('.title', { delay: 1000});
+ScrollReveal().reveal('.cardOne', {delay: 2000});
+ScrollReveal().reveal('.cardTwo', { delay: 2500});
