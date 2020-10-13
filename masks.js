5bea1a556f1130efcea8fc056c8cc2aee5281086
Caroline E <caroline.esquivel01@gmail.com>
2020-10-12 22:40:22 -0500

    shopping cart functionality

--- masks.js
+++ masks.js
@@ -1,6 +1,10 @@
 "use strict";
 
 
+const itemList = document.getElementById('item-list')
+const cartQty = document.getElementById('cart-qty')
+const cartTotal = document.getElementById('cart-total')
+
 const cart = [];
 
 function addItem(name, price) {
@@ -10,30 +14,73 @@ function addItem(name, price) {
             return
         }
     }
-    const item = {name: name, price: price, qty: 1}
+    const item = {name, price, qty: 1}
     cart.push(item)
 }
 
 function showItems() {
+    const qty = getQty();
+
+    console.log(`You have ${qty} items in your cart.`)
+    cartQty.innerHTML = `You have ${qty} items in your cart.`
+
+    let itemStr = ''
+    for (let i = 0; i < cart.length; i += 1) {
+        // console.log(`${cart[i].name} $${cart[i].price} * ${cart[i].qty}`)
+        // const name = cart[i].name
+        // const price = cart[i].price
+        // const qty = cart[i].qty
+
+        const { name, price, qty} = cart[i]
+
+        itemStr += `<li>
+            ${name} 
+            $${price} * ${qty} =
+            ${qty * price}
+            </li>`
+    }
+    itemList.innerHTML = itemStr
+
+    // console.log(`Total in cart: $${getTotal()}`)
+    cartTotal.innerHTML = `Total in cart: $${getTotal()}`
+}
+
+function getQty() {
     let qty = 0
     for (let i = 0; i < cart.length; i += 1) {
         qty += cart [i].qty;
     }
-    console.log(`You have ${qty} items in your cart.`)
+    return qty
+}
 
-    for (let i = 0; i < cart.length; i += 1) {
-        console.log(`${cart[i].name} $${cart[i].price} * ${cart[i].qty}`)
-    }
-
+function getTotal() {
     let total = 0 //store running total
     for (let i = 0; i < cart.length; i += 1) {
         total += cart[i].price * cart[i].qty
     }
-    console.log(`Total in cart: $${total}`)
+    return total.toFixed(2)
 }
+
+function removeItem(name, qty = 0) {
+    for (let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty > 0) {
+                cart[i].qty -= qty
+            }
+            if (cart[i].qty < 1 || qty === 0) {
+                cart.splice(i, 1)
+            }
+            return
+        }
+    }
+}
+
 addItem('apple', 0.99)
 addItem('apple', 0.99)
 addItem('pizza', 4.99)
+// showItems()
+removeItem('pizza')
+removeItem('apple', 1)
 showItems()
 // const sizeSubmit = document.getElementById('toCart');
 // const kids = document.getElementById('kids');
