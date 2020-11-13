020028b0536ff3d2e0738566d147e74a2e6aa33f
Caroline E <caroline.esquivel01@gmail.com>
2020-11-12 22:56:23 -0600

    experiment with logins

---
+++ src/main/java/adlister/controllers/CreateAdServlet.java
@@ -0,0 +1,39 @@
+package adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
+public class CreateAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        if (request.getSession().getAttribute("user") == null) {//this protects from accessing ads/create if user is not logged in
+            response.sendRedirect("/login");
+            return;
+        }
+
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User sessionUser = (User) request.getSession().getAttribute("user");//get user session and cast into User object to get information like getters and setters
+
+        Ad ad = new Ad(
+//            1, // for now we'll hardcode the user id
+                sessionUser.getId(),
+                request.getParameter("title"),
+                request.getParameter("description")
+        );
+        DaoFactory.getAdsDao().insert(ad);
+        response.sendRedirect("/ads");
+    }
+}
